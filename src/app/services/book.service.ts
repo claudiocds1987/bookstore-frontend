d91ce2de7f4e5bafa68d3c11d5025fcfa744cdb8
claudiocds1987 <cla8787@gmail.com>
2021-03-18 14:09:10 -0300

    book.service.ts nueva ruta del server

--- src/app/services/book.service.ts
+++ src/app/services/book.service.ts
@@ -1,15 +1,15 @@
-import { Injectable } from '@angular/core';
+import { Injectable, isDevMode } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Book } from '../models/book';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class BookService {
-
-  URL_API = 'http://localhost:4000/books';
+ // URL_API = 'http://localhost:4000/books';
+  SERVER = 'http://localhost:3000';
   // objeto vacio tipo Book
-    book: Book = {
+  book: Book = {
     name: '',
     year: 0,
     id_author: 0,
@@ -20,86 +20,120 @@ export class BookService {
     price: 0,
     url_image: '',
     state: true,
-    id_book: 0
+    id_book: 0,
   };
 
   bookArray: Book[]; // array de tipo Book
 
-  constructor(private http: HttpClient) { }
-
-  getBookById(id: string){
-    return this.http.get<Book[]>(`${this.URL_API}/${id}`);
+  constructor(private http: HttpClient) {
+    if (!isDevMode()) {
+      this.SERVER = 'https://bookstore-cds-server.herokuapp.com';
+    }
   }
 
-  getImage(){
-    return this.http.get<any>('http://localhost:4000/1f2d312a-a1ef-48c5-a79f-c2a27c48320c.jpg');
+  getBookById(id: string) {
+   // return this.http.get<Book[]>(`${this.URL_API}/${id}`);
+    return this.http.get<Book[]>(`${this.SERVER}/${id}`);
   }
 
-  // trate todos los sin importar su estado state
+  getImage() {
+    return this.http.get<any>(
+      //'http://localhost:4000/1f2d312a-a1ef-48c5-a79f-c2a27c48320c.jpg'
+      'https://bookstore-cds-server.herokuapp.com/1f2d312a-a1ef-48c5-a79f-c2a27c48320c.jpg'
+    );
+  }
+
+  // trae todos los libros sin importar su estado state
   getBooksWithAuthorName() {
-    // http://localhost:4000/booksAuthorName
-    return this.http.get<Book[]>(this.URL_API + 'AuthorName');
+    // return this.http.get<Book[]>(this.URL_API + 'AuthorName');
+    return this.http.get<Book[]>(`${this.SERVER}/books/AuthorName`);
   }
 
   // trae solo los libros que tiene state = true (aptos parta la venta)
   getAvailableBooksWithAuthorName() {
-    return this.http.get<Book[]>('http://localhost:4000/AvailableBooksWithAuthorName');
+    return this.http.get<Book[]>(
+      //'http://localhost:4000/AvailableBooksWithAuthorName'
+      `${this.SERVER}/AvailableBooksWithAuthorName`
+    );
   }
 
   getOneBookWithAuthorName(id: string) {
-    return this.http.get<Book[]>(`${'http://localhost:4000/bookAuthorName'}/${id}`);
+    return this.http.get<Book[]>(
+      // `${'http://localhost:4000/bookAuthorName'}/${id}`
+      `${this.SERVER}/bookAuthorName/${id}`
+    );
   }
 
   getRealDataBook(id: string) {
     // trae toda la data de book
     // mas nombre de autor, nombre de editorial y nombre de categoria que no estan en el model Book
     // por eso de tipo "any"
-    return this.http.get<any[]>(`${'http://localhost:4000/getRealDataBook'}/${id}`);
+    return this.http.get<any[]>(
+      // `${'http://localhost:4000/getRealDataBook'}/${id}`
+      `${this.SERVER}/getRealDataBook/${id}`
+    );
   }
 
   getBooks() {
-    return this.http.get<Book[]>(this.URL_API);
+    // return this.http.get<Book[]>(this.URL_API);
+    return this.http.get<Book[]>(`${this.SERVER}/books`);
   }
 
-  existBook(bookName: string, idAuthor: number){
-    return this.http.get('http://localhost:4000/books/exist/' + bookName + '/' + idAuthor);
+  existBook(bookName: string, idAuthor: number) {
+    return this.http.get(
+      //'http://localhost:4000/books/exist/' + bookName + '/' + idAuthor
+      `${this.SERVER}/books/exist/` + bookName + '/' + idAuthor
+    );
   }
 
-  createBook(book: Book){
-    // console.log(book);
-    return this.http.post(this.URL_API, book);
+  createBook(book: Book) {
+    // return this.http.post(this.URL_API, book);
+    return this.http.post(`${this.SERVER}/books`, book);
   }
 
-  updateBook(book: Book){
-    // console.log('Ca update book: ' + book.name);
-    return this.http.put(`${this.URL_API}/${book.id_book}`, book);
+  updateBook(book: Book) {
+    // return this.http.put(`${this.URL_API}/${book.id_book}`, book);
+    return this.http.put(`${this.SERVER}/books/${book.id_book}`, book);
   }
 
-  filterAvailableBooksByName(name: string){
-    return this.http.get<Book[]>('http://localhost:4000/filterAvailableBooksByName/' + name);
+  filterAvailableBooksByName(name: string) {
+    return this.http.get<Book[]>(
+      // 'http://localhost:4000/filterAvailableBooksByName/' + name
+      `${this.SERVER}/filterAvailableBooksByName/` + name
+    );
   }
 
-  filterBooksByName(name: string){
-    return this.http.get<Book[]>('http://localhost:4000/filterBooksByName/' + name);
+  filterBooksByName(name: string) {
+    return this.http.get<Book[]>(
+     // 'http://localhost:4000/filterBooksByName/' + name
+     `${this.SERVER}/filterBooksByName/` + name
+    );
   }
 
-  filterAvailableBooksByAuthor(name: string){
-    return this.http.get<Book[]>('http://localhost:4000/filterAvailableBooksByAuthor/' + name);
+  filterAvailableBooksByAuthor(name: string) {
+    return this.http.get<Book[]>(
+      // 'http://localhost:4000/filterAvailableBooksByAuthor/' + name
+      `${this.SERVER}/filterAvailableBooksByAuthor/` + name
+    );
   }
 
-  filterBooksByAuthor(name: string){
-    return this.http.get<Book[]>('http://localhost:4000/filterBooksByAuthor/' + name);
+  filterBooksByAuthor(name: string) {
+    return this.http.get<Book[]>(
+      // 'http://localhost:4000/filterBooksByAuthor/' + name
+      `${this.SERVER}/filterBooksByAuthor/` + name
+    );
   }
 
-  bajaBook(idBook: number, changes: Partial<Book>){
-   const url = 'http://localhost:4000/books/baja/';
-   return this.http.put(`${url}${idBook}`, changes);
-  }
-
-  altaBook(idBook: number, changes: Partial<Book>){
-    const url = 'http://localhost:4000/books/alta/';
+  bajaBook(idBook: number, changes: Partial<Book>) {
+   // const url = 'http://localhost:4000/books/baja/';
+   const url = `${this.SERVER}/books/baja/`;
     return this.http.put(`${url}${idBook}`, changes);
-   }
+  }
 
+  altaBook(idBook: number, changes: Partial<Book>) {
+    // const url = 'http://localhost:4000/books/alta/';
+    const url = `${this.SERVER}/books/alta/`;
+    return this.http.put(`${url}${idBook}`, changes);
+  }
 
 }
