75e5823f9f1425f94a5834efb14cac61ade531ad
claudiocds1987 <cla8787@gmail.com>
2021-04-14 15:32:05 -0300

    spinner loader en user y admin login

--- src/app/user/auth/auth.module.ts
+++ src/app/user/auth/auth.module.ts
@@ -5,6 +5,9 @@ import { MaterialModule } from './../../material/material.module';
 import { AuthRoutingModule } from './auth-routing.module';
 import { UserLoginComponent } from './user-login/user-login.component';
 import { UserSignupComponent } from './user-signup/user-signup.component';
+// imports para el funcionamiento del spinner/loader para peticiones http
+import { NgxSpinnerModule } from 'ngx-spinner';
+// import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
   declarations: [
@@ -16,6 +19,7 @@ import { UserSignupComponent } from './user-signup/user-signup.component';
     AuthRoutingModule,
     FormsModule,
     ReactiveFormsModule,
+    NgxSpinnerModule,
     MaterialModule
   ]
 })
